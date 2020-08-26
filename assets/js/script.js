c6e0f8e97612dfd6301a9dc8d59e95522e6a570c
Blue Abad <bibo1011@gmail.com>
2020-08-25 21:59:45 -0700

    add search input

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var cityInputEl = document.getElementById("#city")
+
+var formSubmitHandler = function(event){
+    event.preventDefault();
+    //get value of input element
+    var city = cityInputEl.value.trim();
+
+    if (city) {
+        
+    } 
+}
