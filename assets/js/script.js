6cf2a238884cbcc4bec791fc654caeb748363c64
Blue Abad <bibo1011@gmail.com>
2020-08-27 17:43:55 -0700

    add show history

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,6 @@
-// Set-Cookie: __cfduid=d519cd78113d4f0674d45c26bf3efc9531571611678; SameSite=None
-// var getCityRepos = function(city){
-//     var cityEl = document.getElementById("#city").value;
 
-//     //format openweather url
-//     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=7cb9d1b9e692149f5ce8f4913f261ae6";
-//     // make a request to the url
-//     fetch(apiUrl).then(function(response){
-//         response.json()
-//         .then(function(data){
-//             displayRepos(data, city);
-//         });
-//         console.log(response);
-//     });
-// };
-function myFunction (){
-    
+// search 
+function citySearch (){
     var searchCity = document.querySelector("#city").value;
 
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + searchCity + '&appid=7cb9d1b9e692149f5ce8f4913f261ae6')
@@ -25,22 +11,32 @@ function myFunction (){
     .then(function(response){
         console.log(response);
     })
-}
-// var cityFormEl = document.querySelector("#city-form");
 
-// var formSubmitHandler = function(event){
-
-//     event.preventDefault();
-//     //get value of input element
-//     var city = cityInputEl.value.trim();
-
-//     if (city) {
-//     getCityRepos(city);
-//     cityInputEl.value = "";
-//     } else {
-//     alert("Please enter a city");
-//     }
-//     console.log(event);
+    // history
+    localStorage.setItem("city", searchCity);
+    console.log(searchCity);
+    // document.getElementById("history-container").innerHTML = searchCity;
     
-// }
-// cityFormEl.addEventListener('submit', formSubmitHandler);
+    // var x = document.createElement("P");
+    // var t = document.createTextNode(searchCity);
+    // x.appendChild(t);
+    // document.body.div.div.div.appendChild(x);
+
+    document.getElementById("history").innerHTML +=
+    "<div> <div class=\"p\">" + searchCity
+   
+    
+
+
+
+
+
+
+
+    // var showHistory = localStorage.getItem("city");
+
+    
+    // var ssearchHistory = document.createElement("li")
+    // ssearchHistory.style.
+    // ssearchHistory.setAttribute('src', searchCity)
+}
